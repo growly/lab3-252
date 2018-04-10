8ca984b3ab6a82ca2650615a8f802b6de4bb4a9c
nqdtan <tan.nqd@gmail.com>
2018-04-09 21:24:37 -0700

    add test for correctness for benchmark simple

--- test/riscv-bmarks/simple/simple.c
+++ test/riscv-bmarks/simple/simple.c
@@ -4,6 +4,15 @@
 #define SIZE 1000
 typedef int DATATYPE;
 
+int test(DATATYPE *arr1, DATATYPE *arr2, int k) {
+  int i;
+  for (i = 0; i < SIZE; i++)
+    if (arr2[i] != arr1[i] * k + 1)
+      return 1;
+
+  return 0;
+}
+
 __attribute__ ((noinline)) void simple(DATATYPE *arr1, DATATYPE *arr2, int k) {
   int i;
   for (i = 0; i < SIZE; i+=1) {
@@ -22,5 +31,5 @@ int main() {
   simple(arr1, arr2, 2);
   setStats(0);
 
-  return 0;
+  return test(arr1, arr2, 2);
 }
