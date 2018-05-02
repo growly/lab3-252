29d5645c67dc7a1fc703c2fd117584d137a8f2d5
nqdtan <tan.nqd@gmail.com>
2018-05-02 03:06:03 -0700

    add benchmark vecadd

---
+++ test/riscv-bmarks/vecadd/vecadd.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "util.h"
+
+#define SIZE 1000
+typedef int DATATYPE;
+
+__attribute__ ((noinline)) int test(int n, DATATYPE *arr1, DATATYPE *arr2,
+  DATATYPE *arr3) {
+  int i;
+  for (i = 0; i < n; i+=1) {
+    if (arr3[i] != arr1[i] + arr2[i])
+      return 1;
+  }
+
+  return 0;
+}
+
+__attribute__ ((noinline)) void vecadd(int n, DATATYPE *arr1, DATATYPE *arr2,
+  DATATYPE *arr3) {
+  int i;
+  for (i = 0; i < n; i+=1) {
+    arr3[i] = arr1[i] + arr2[i];
+  }
+}
+
+int main() {
+  DATATYPE arr1[SIZE];
+  DATATYPE arr2[SIZE];
+  DATATYPE arr3[SIZE];
+
+  int i;
+  for (i = 0; i < SIZE; i++) {
+    arr1[i] = i;
+    arr2[i] = i;
+  }
+
+  setStats(1);
+  vecadd(SIZE, arr1, arr2, arr3);
+  setStats(0);
+
+  return test(SIZE, arr1, arr2, arr3);
+}
