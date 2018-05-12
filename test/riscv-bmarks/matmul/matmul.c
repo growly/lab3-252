43b23293fc4f36c1a717ef5fed8e405e299ff3c2
nqdtan <tan.nqd@gmail.com>
2018-05-11 23:18:40 -0700

    add benchmark matmul (from arya)

---
+++ test/riscv-bmarks/matmul/matmul.c
@@ -0,0 +1,53 @@
+// See LICENSE for license details.
+
+//**************************************************************************
+// Inspired by the Multi-threaded Matrix Multiply benchmark by C. Celio.
+//--------------------------------------------------------------------------
+//
+// This benchmark multiplies two matrices (2-D arrays) together, or adds them,
+// depending on the values in a third vector.
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+// This should typedef data_t for us.
+#include "dataset.h"
+ 
+void matrix_multiply(
+    const size_t n, const data_t A[], const data_t B[], data_t R[] ) {
+  size_t i, j, k;
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < n; ++j) {
+      int idx = j * n + i;
+      int c = 0;
+      for (k = 0; k < n; ++k) {
+        c += A[j * n + k] * B[k * n + i];
+      }
+      R[idx] = c;
+    }
+  }
+}
+
+
+int verify(const size_t n, const data_t expected[], const data_t actual[]) {
+  size_t i;
+  for (i = 0; i < n; ++i)
+    if (expected[i] != actual[i])
+      return 1;
+  return 0;
+}
+
+int main(int argc, char **argv) {
+   static data_t results_data[ARRAY_SIZE];
+
+   // stats();
+
+   matrix_multiply(
+       DIM_SIZE, input_a, input_b, results_data);
+
+   int res = verify(ARRAY_SIZE, input_verify, results_data);
+
+   exit(res);
+}
